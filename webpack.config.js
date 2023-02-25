ec2c124a0ba2f4d11d613f9f49152d13b99cd8ac
kamranahmad123 <kamrankhan.ahmad2@yahoo.com>
2023-02-25 05:32:30 -0800

    remove linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      title: 'Development',
     }),
   ],
   output: {
