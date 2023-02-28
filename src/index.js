e9da597e4889db4d7b95f779d868ae7e003d11d6
kamranahmad123 <kamrankhan.ahmad2@yahoo.com>
2023-02-28 00:39:27 -0800

    create interactive todo list

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import './style.css';
 import {
   addButton, getData, inputTask, tasksData, executeData,
 } from './newFunctionalities.js';
+import deleteMultiple from './deleteSelected.js';
+
+const clearButton = document.querySelector('.clear');
+
+const refreshButton = document.querySelector('.refreshButton');
+
+refreshButton.addEventListener('click', () => window.location.reload());
 
 addButton.addEventListener('click', () => {
   getData(inputTask);
 });
 
+clearButton.addEventListener('click', deleteMultiple);
 executeData(tasksData);
--- src/newFunctionalities.js
+++ src/newFunctionalities.js
@@ -1,25 +1,30 @@
+import checkDelete from './completionStatus.js';
+
 export const inputTask = document.getElementById('input-area');
 export const addButton = document.querySelector('.addButton');
 export const tasksData = JSON.parse(localStorage.getItem('task')) || [];
 let filterData = [];
 const taskRemover = (tasknumber) => {
-  filterData = tasksData.filter((dailyTasks) => dailyTasks.index !== tasknumber) || [];
+  filterData = tasksData.filter((dailyTasks) => dailyTasks.index !== tasknumber.index) || [];
   localStorage.setItem('task', JSON.stringify(filterData));
   window.location.reload();
 };
+
 export function executeData(input) {
   input.forEach((element, i) => {
     element.index = i + 1;
+
     const listEntry = document.querySelector('.list-entry');
     const li = document.createElement('li');
     const task = document.createElement('input');
     const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkBox');
     const deleteButton = document.createElement('i');
     deleteButton.setAttribute('class', 'fa-solid fa-trash');
-    checkBox.setAttribute('type', 'checkBox');
-    checkBox.value = `${element.completed}`;
     task.value = `${element.description}`;
-    deleteButton.onclick = () => taskRemover(element.index);
+    deleteButton.onclick = () => taskRemover(element);
+    checkBox.onclick = () => checkDelete(element, tasksData);
+    checkBox.checked = element.completed;
     li.classList.add('list');
     li.classList.add('border');
     deleteButton.classList.add('deleteButton');
@@ -30,9 +35,9 @@ export function executeData(input) {
     listEntry.appendChild(li);
   });
 }
-
-export const getData = (Tasks) => {
-  tasksData.push({ description: Tasks.value, completed: false, index: null });
+export const getData = () => {
+  const objData = { description: inputTask.value, completed: false, index: null };
+  tasksData.push(objData);
   localStorage.setItem('task', JSON.stringify(tasksData));
   window.location.reload();
 };
