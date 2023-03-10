c97b7a045b0e6844c5e80ec0b845bc912c4dd79f
kamranahmad123 <kamrankhan.ahmad2@yahoo.com>
2023-03-10 10:01:05 -0800

    test update status, descriptiona and delete multiple functions

--- src/main.test.js
+++ src/main.test.js
@@ -1,8 +1,10 @@
 /**
  * @jest-environment jsdom
  */
+import checkDelete from './completionStatus.js';
+import deleteMultiple from './deleteSelected.js';
 import {
-  getData, getStorage, taskRemover,
+  getData, getStorage, taskRemover, updateDescription,
 } from './newFunctionalities.js';
 
 describe('Add-to-do method', () => {
@@ -18,10 +20,27 @@ describe('Add-to-do method', () => {
 });
 
 describe('Remove Method', () => {
-  it('remove task to-do', () => {
+  it('remove task from to-do list', () => {
     // act
     taskRemover(1);
     // assert
     expect(getStorage()).toHaveLength(3);
+  });
+});
+describe('Update description Method', () => {
+  it('update tasks', () => {
+    // act
+    updateDescription('Swim', '1');
+    // assert
+    expect(getStorage()[0].description).toBe('Swim');
+  });
+});
+
+describe('Update status Method', () => {
+  it('update tasks status', () => {
+    // act
+    checkDelete(1, getStorage());
+    // assert
+    expect(getStorage()[0].completed).toBe(true);
   });
 });
